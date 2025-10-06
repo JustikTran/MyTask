eba2a30e7a544269aae92e8e30cb88092731b991
ChinhTC <assin2910@gmail.com>
2025-10-06 11:11:08 +0700

    feat: config lib and DI

---
+++ API/Program.cs
@@ -0,0 +1,141 @@
+using API.Application.DTOs.TaskItem;
+using API.Application.DTOs.User;
+using API.Application.Services;
+using API.Domain.Interfaces;
+using API.Infrastructure.Data;
+using API.Infrastructure.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OData.ModelBuilder;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+// Register OData model builder
+var odataBuilder = new ODataConventionModelBuilder();
+
+odataBuilder.EntitySet<UserResponse>("user");
+odataBuilder.EntitySet<TaskItemResponse>("task-item");
+
+builder.Services.AddControllers()
+    .AddOData(options => options
+        .SetMaxTop(100)
+        .Filter()
+        .OrderBy()
+        .Count()
+        .Expand()
+        .Select()
+        .AddRouteComponents("odata", odataBuilder.GetEdmModel())
+        )
+    .AddJsonOptions(opt => opt.JsonSerializerOptions
+    .PropertyNamingPolicy = JsonNamingPolicy.CamelCase);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var jwtSecuritySchema = new OpenApiSecurityScheme
+    {
+        BearerFormat = "JWT",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Enter your JWT Access Token",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    options.AddSecurityDefinition("Bearer", jwtSecuritySchema);
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecuritySchema, Array.Empty<string>() }
+    });
+});
+
+// Config JWT Authentication
+var key = Encoding.UTF8.GetBytes(builder.Configuration["JwtConfig:Key"]!);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = true;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = builder.Configuration["JwtConfig:Issuer"],
+        ValidAudience = builder.Configuration["JwtConfig:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+// Add Authorization
+builder.Services.AddAuthorization();
+
+// Add DI for application services
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ITaskItemRepository, TaskItemRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var service = scope.ServiceProvider;
+    var context = service.GetRequiredService<AppDbContext>();
+    //context.Database.EnsureDeleted();
+    context.Database.GetMigrations();
+    context.Database.Migrate();
+}
+
+app.UseHttpsRedirection();
+
+app.UseODataRouteDebug();
+
+app.UseCors("AllowAll");
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
